c56bddcd23f11364efa01caff4a5db9e8d02b87f
Mario Marroquín <mariomarroquin1905@gmail.com>
2023-03-26 20:51:51 -0600

    Actualización: doc, eliminación listas, nuevas def

--- Proyecto/compuestos/compuestos.py
+++ Proyecto/compuestos/compuestos.py
@@ -7,9 +7,9 @@ from compuestos.EleCompuesto import Lista_EleCompuesto
 elemento_Compuesto = Lista_EleCompuesto()
 
 class Compuesto:
-    def __init__(self, nombre, formula):
+    def __init__(self, nombre):#, formula
         self.nombre = nombre
-        self.formula = formula
+        #self.formula = formula
 
 class nodoCompuesto:
     def __init__(self, compuesto, siguiente = None, anterior = None):
@@ -18,49 +18,41 @@ class nodoCompuesto:
         self.anterior = anterior
 
 class listaDobleCompuesto:
+
     def __init__(self):
         self.primero = None
-    
+        self.ultimo = None
+
     def insertar(self, compuesto):
-        for elemento in compuesto.formula:
-            elemento_Compuesto.insertar(elemento)
+        '''for elemento in compuesto.formula:
+            elemento_Compuesto.insertar(elemento)'''
+        nuevo_nodo = nodoCompuesto(compuesto = compuesto)
         if self.primero is None:
-            self.primero = nodoCompuesto(compuesto = compuesto)
+            self.primero = nuevo_nodo
+            self.ultimo = nuevo_nodo
         else:
-            actual = nodoCompuesto(compuesto = compuesto, siguiente = self.primero)
-            self.primero.anterior = actual
-            self.primero = actual        
-    
-    def verCompuesto_Formulas(self, ventana):
+            nuevo_nodo.siguiente = self.primero
+            self.primero.anterior = nuevo_nodo
+            self.primero = nuevo_nodo
+     
+    def verCompuesto_Formulas(self, ventana, elemento_Compuesto):
         self.ver = Text(ventana, width=50, height=10, **style.STYLE)
         self.ver.tag_config("orange", foreground="orange")
         self.ver.tag_config("white", foreground="white")
-        if self.primero is None:
-            return
-        actual = self.primero
-        self.ver.insert("1.0", "\n--COMPUESTO-- \n", "orange")
-        self.ver.insert("end",'    '+str(actual.compuesto.nombre)+'\n',"white")
-        self.ver.insert("end",'\n-Formulas:\n', "orange")
-        self.ver.insert("end",'    '+str(elemento_Compuesto.buscarElementos(str(actual.compuesto.nombre)))+'\n', "white")
-        while actual.siguiente:
-            actual = actual.siguiente
+        actual = self.ultimo
+        while actual:
             self.ver.insert("end", "\n--COMPUESTO-- \n", "orange")
             self.ver.insert("end",'    '+str(actual.compuesto.nombre)+'\n',"white")
             self.ver.insert("end",'\n-Formulas:\n', "orange")
             self.ver.insert("end",'    '+str(elemento_Compuesto.buscarElementos(str(actual.compuesto.nombre)))+'\n', "white")
+            actual = actual.anterior
         self.ver.configure(state="disabled")
         self.ver.pack(side=tk.TOP,fill=tk.BOTH,expand=True,padx=22,pady=11)
-
+        
     def verFormula_Compuestos(self):
         self.ver.pack(side=tk.TOP,fill=tk.BOTH,expand=True,padx=22,pady=11)
 
-
     def vaciar(self):
-        actual = self.primero
-        while actual:
-            siguiente = actual.siguiente
-            del actual
-            actual = siguiente
         self.primero = None
-        elemento_Compuesto.vaciar()
-        
+        self.ultimo = None
+
